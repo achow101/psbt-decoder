9adf1e5c214ece218b5fb47cc894102720413eb9
Andrew Chow <achow101-github@achow101.com>
2020-11-18 16:18:50 -0500

    Black formatting

--- decoder.py
+++ decoder.py
@@ -10,14 +10,19 @@ from io import BytesIO
 
 parser = argparse.ArgumentParser(description="Decode a PSBT")
 parser.add_argument("--hex", action="store_true", help="psbt is in hex, not base64")
-parser.add_argument("--psbt-types", help="JSON file that contains all of the types in the PSBT")
-parser.add_argument("--pset", action="store_true", help="The PSBT is actually for Elements, it's a PSET")
+parser.add_argument(
+    "--psbt-types", help="JSON file that contains all of the types in the PSBT"
+)
+parser.add_argument(
+    "--pset", action="store_true", help="The PSBT is actually for Elements, it's a PSET"
+)
 parser.add_argument("psbt", help="psbt to decode")
 
 args = parser.parse_args()
 
 num_inputs = 0
 num_outputs = 0
+
 
 def read_csuint(s):
     size = struct.unpack("<B", s.read(1))[0]
@@ -29,9 +34,11 @@ def read_csuint(s):
         return struct.unpack("<Q", s.read(8))[0]
     return size
 
+
 def read_bitcoin_vec(s):
     size = read_csuint(s)
     return size, s.read(size)
+
 
 def deser_map(s, scope, count=None):
     c_str = "" if count is None else f"{count} "
@@ -73,23 +80,26 @@ def deser_map(s, scope, count=None):
         # Read the value
         value_size, value_data = read_bitcoin_vec(s)
         if is_tx:
-            s_val = BytesIO(value_data);
-            s_val.read(4) # TX Version
+            s_val = BytesIO(value_data)
+            s_val.read(4)  # TX Version
             global num_inputs
             global num_outputs
             num_inputs = read_csuint(s_val)
 
             # Skip the rest to get to the output count
             for _ in range(num_inputs):
-                s_val.read(36) # Outpoint
+                s_val.read(36)  # Outpoint
                 script_size = read_csuint(s_val)
-                s_val.read(script_size) # scriptSig
-                s_val.read(4) # Sequence
+                s_val.read(script_size)  # scriptSig
+                s_val.read(4)  # Sequence
 
             num_outputs = read_csuint(s_val)
 
         # Print these out
-        print(f"RECORD:\t\t{psbt_type.upper()}\t{key_size}\t{key_data.hex()}\t{value_size}\t{value_data.hex()}")
+        print(
+            f"RECORD:\t\t{psbt_type.upper()}\t{key_size}\t{key_data.hex()}\t{value_size}\t{value_data.hex()}"
+        )
+
 
 if args.hex:
     # Hex decode
